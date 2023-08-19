4679e3918cd8982804cbc59d5b94b02acdc7c5ce
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-08-19 21:46:31 +0900

    ヘッダー作成

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
+import styles from '../styles/App.module.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const login = () => {
+    alert("aaaa");
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          Otoko
+        </h1>
+        <div className={styles.header_items}>
+          <button className={styles.login_button} onClick={login}>
+            ログイン
+          </button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
