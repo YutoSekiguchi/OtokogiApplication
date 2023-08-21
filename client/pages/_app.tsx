e8eb5f56a0ada3324d98b7cbb08a1f7634d0b29e
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-08-21 23:04:46 +0900

    修正

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,16 @@
-import '../styles/globals.css'
 import styles from '../styles/App.module.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import Header from '../components/common/Header'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <noscript id="__next_css__DO_NOT_USE__" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <footer className={styles.footer}>
