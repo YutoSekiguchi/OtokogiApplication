b6d167a7cc12976238afd55c09c8a8aa8a3e94d2
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-08-23 22:54:38 +0900

    環境変数の追加

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
+  },
+  env: {
+    API_URL: process.env.MODE=="production" ? process.env.API_URL_PROD : process.env.API_URL_DEV,
   },
 }
 
