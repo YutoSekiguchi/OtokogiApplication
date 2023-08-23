e51f1795564ec3219f3f83aa421cc72c0c548ef0
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-08-23 22:56:31 +0900

    callbackの追加

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PostUserDataType } from '../../../@types/user';
+import { getFriendCode } from '../../../modules/getFriendCode';
+import { signin } from '../../../services/user';
 
 type ClientType = {
   clientId: string;
@@ -14,6 +17,28 @@ const authOptions: NextAuthOptions = {
     } as ClientType),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn(params) {
+      const profile = params.profile;
+      if (profile === undefined) {
+        return Promise.resolve(false);
+      }
+      const friendCode = getFriendCode();
+      const data: PostUserDataType = {
+        name: profile.name!,
+        displayName: profile.name!,
+        mail: profile.email!,
+        image: params.user.image!,
+        friendCode: friendCode
+      }
+      const userData = await signin(data);
+      //TODO: imageが違う場合の処理も記述
+      if(userData === null || userData === undefined) {
+        return Promise.resolve(false);
+      }
+      return Promise.resolve(true);
+    },
+  }
 };
 
 export default NextAuth(authOptions);
