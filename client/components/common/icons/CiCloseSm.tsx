a877e89b1e4bbf4e74e609a24dc6b5d713f78cf3
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-08-24 17:14:43 +0900

    iconの追加

---
+++ client/components/common/icons/CiCloseSm.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react"
+
+export function CiCloseSm(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m16 16l-4-4m0 0L8 8m4 4l4-4m-4 4l-4 4"></path></svg>
+  )
+}
