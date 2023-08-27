d861384934559e6787b0bc858415dffeb2ac8541
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-08-27 11:53:22 +0900

    iconの追加

---
+++ client/components/common/icons/OiAccountLogout.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react"
+
+export function OiAccountLogout(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 8 8" {...props}><path fill="currentColor" d="M3 0v1h4v5H3v1h5V0H3zM2 2L0 3.5L2 5V4h4V3H2V2z"></path></svg>
+  )
+}
