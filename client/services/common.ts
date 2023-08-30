02bcfd1c562cf03e8cb7c084f657e57436cdf998
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-08-30 22:04:51 +0900

    返り値の変更

--- client/services/common.ts
+++ client/services/common.ts
@@ -2,6 +2,9 @@ import { AxiosResponse } from "axios";
 
 export const returnData = (res: AxiosResponse<any, any>) => {
   if(res.status === 200) {
+    if (res.data === "") {
+      return null;
+    }
     return res.data;
   } else {
     console.log(res);
