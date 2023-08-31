8f0bbf9d9d588db41f195b3467fe87843a39b6a4
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-08-31 14:35:21 +0900

    検索機能をフレンドに対応

--- client/modules/generateSuggestion.ts
+++ client/modules/generateSuggestion.ts
@@ -1,5 +1,6 @@
-export const generateSuggestions = (input: string, suggestionList: string[]) => {
-  return  suggestionList.filter(suggestion =>
-    suggestion.toLowerCase().includes(input.toLowerCase())
-  );
+import { UserDataType } from "../@types/user";
+
+export const generateSuggestions = (input: string, suggestionList: UserDataType[]) => {
+  const lowercaseTarget = input.toLowerCase();
+  return suggestionList.filter(obj => obj.displayName!.toLowerCase().includes(lowercaseTarget));
 };
