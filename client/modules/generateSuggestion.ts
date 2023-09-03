0d793746463917fab97689bc4e567dabbffb1d02
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-03 22:10:19 +0900

    検索方法の変更

--- client/modules/generateSuggestion.ts
+++ client/modules/generateSuggestion.ts
@@ -1,6 +1,20 @@
-import { UserDataType } from "../@types/user";
+import { UserDataType, memberNameAndIDType } from "../@types/user";
 
-export const generateSuggestions = (input: string, suggestionList: UserDataType[]) => {
+export const generateSuggestions = (input: string, suggestionList: UserDataType[], memberList: memberNameAndIDType[]) => {
   const lowercaseTarget = input.toLowerCase();
-  return suggestionList.filter(obj => obj.displayName!.toLowerCase().includes(lowercaseTarget));
+  const suggestionNameList = suggestionList.filter(obj => obj.displayName!.toLowerCase().includes(lowercaseTarget));
+  var res = [];
+  for(let i=0; i<suggestionNameList.length; i++) {
+    var count = 0;
+    for(let j=0; j<memberList.length; j++) {
+      if(suggestionNameList[i].id === memberList[j].id) {
+        count += 1
+      }
+    }
+    if (count == 0) {
+      res.push(suggestionNameList[i]);
+    }
+    count = 0;
+  }
+  return res;
 };
