70c3904df3d63353c0e3eb883d28de44f37729aa
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-12 21:13:06 +0900

    デザイン修正

--- client/components/record/ShareButton.tsx
+++ client/components/record/ShareButton.tsx
@@ -1,29 +1,13 @@
 import { useState } from 'react';
 import styles from "../../styles/Reccord.module.css"
+import { IcSharpOpenInNew } from '../common/icons/IcSharpOpenInNew';
 
 const ShareButton: React.FC<{ textToShare: string }> = ({ textToShare }) => {
-  // const [copied, setCopied] = useState(false);
-
-  // const handleCopy = () => {
-  //   const textArea = document.createElement('textarea');
-  //   textArea.value = textToShare;
-  //   document.body.appendChild(textArea);
-  //   textArea.select();
-
-  //   try {
-  //     document.execCommand('copy');
-  //     setCopied(true);
-  //   } catch (err) {
-  //     console.error('コピーに失敗しました:', err);
-  //   } finally {
-  //     document.body.removeChild(textArea);
-  //   }
-  // };
-
   return (
     <button
       className={styles.copy_button}
     >
+      <IcSharpOpenInNew />
       <a
         href={`https://social-plugins.line.me/lineit/share?url=${textToShare}`}
         className={styles.share_text}
