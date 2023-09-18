9ccabde51e6cc81eac44caad7251284608b840ad
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-18 17:10:57 +0900

    payに関するAPI通信機能の追加

--- client/services/pay.ts
+++ client/services/pay.ts
@@ -4,6 +4,16 @@ import { returnData } from "./common";
 
 const PAY_API_URL = `${process.env.API_URL}/pays`;
 
+export const getPays = async(rid: number) => {
+  const url = `${PAY_API_URL}/${rid}/rid`;
+  try {
+    const res = await axios.get(url);
+    return returnData(res);
+  } catch(error) {
+    throw(error);
+  }
+}
+
 export const postPay = async(data: PostPayDataType) => {
   try {
     const res = await axios.post(PAY_API_URL, data);
