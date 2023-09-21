c51642614d21e9e9e66e06d8ac1e2cf059d55e41
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-21 09:58:29 +0900

    fix

--- client/pages/record/[code]/edit.tsx
+++ client/pages/record/[code]/edit.tsx
@@ -1,1 +1,12 @@
-const 
+import { NextPage } from "next";
+import EditForm from "../../../components/record/EditForm";
+
+const RecordEditPage: NextPage = () => {
+  return (
+    <div className="container">
+      <EditForm mode="edit" />
+    </div>
+  )
+}
+
+export default RecordEditPage;
--- client/pages/record/[code]/payment/[id]/edit.tsx
+++ client/pages/record/[code]/payment/[id]/edit.tsx
@@ -1,24 +1,7 @@
 import { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useRecordStore } from "../../../../../stores/record";
-import { useMemberStore } from "../../../../../stores/member";
-import { useState } from "react";
 import PayEditForm from "../../../../../components/record/PayEditForm";
 
 const PayEditPage: NextPage = () => {
-  const router = useRouter();
-  const record = useRecordStore((state) => state.record);
-  const setRecordData = useRecordStore((state) => state.setRecordData);
-  const setMembers = useMemberStore((state) => state.setMembers);
-  const members = useMemberStore((state) => state.members);
-  const [paidUserId, setPaidUserId] = useState<number>(0);
-  const [paidMemberId, setPaidMemberId] = useState<number>(0);
-  const [isCheckedList, setIsCheckedList] = useState<boolean[]>([]);
-  const [paidEventName, setPaidEventName] = useState<string>("");
-  const [paidMoney, setPaidMoney] = useState<number>(0);
-  const [isCheckDrive, setIsCheckDrive] = useState<boolean>(false);
-  const [isCheckDriveBeer, setIsCheckDriveBeer] = useState<boolean>(false);
-
   return (
     <div className="container">
       <PayEditForm mode="edit" />
