7d150dd171fa77e4f085c38777c780e5ef7bb04b
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-22 13:57:33 +0900

    fix

--- client/modules/edit/findDifferentObjects.ts
+++ client/modules/edit/findDifferentObjects.ts
@@ -1,21 +1,25 @@
-export const findDifferentObjects = (array1: any[], array2: any[]) => {
+import { memberNameAndIDType } from "../../@types/user";
+
+export const findDifferentObjects = (array1: memberNameAndIDType[], array2: memberNameAndIDType[]) => {
   // 条件に一致するオブジェクトを格納する配列
   let differentObjects = [];
 
   // 両方の配列を反復処理
   for (let obj1 of array1) {
-      for (let obj2 of array2) {
-          // idがnull以外でidが異なるもしくはidがnullで名前が異なる場合
-          if (
-              (obj1.id !== null && obj2.id !== null && obj1.id !== obj2.id) ||
-              (obj1.id === null && obj2.id !== null) ||
-              (obj1.id !== null && obj2.id === null) ||
-              obj1.name !== obj2.name
-          ) {
-              differentObjects.push(obj1);
-              break; // 条件に一致するオブジェクトが見つかったらループを抜けます
-          }
-      }
+    let count = 0
+    if (obj1.id === null) {
+        if (array2.some(obj2 => obj2.displayName === obj1.displayName)) {
+            count += 1
+        }
+    } else {
+        if (array2.some(obj2 => obj2.id === obj1.id)) {
+            count += 1
+        }
+    }
+
+    if (count == 0) {
+        differentObjects.push(obj1);
+    }
   }
 
   return differentObjects;
