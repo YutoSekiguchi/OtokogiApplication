9f1a7569a4f2399e901205a4dbcd777ace0d1b9e
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-09-23 12:16:47 +0900

    削除APIの追加

--- api/src/main/java/com/example/api/controllers/PayController.java
+++ api/src/main/java/com/example/api/controllers/PayController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -77,5 +78,15 @@ public class PayController {
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+	// 削除
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deletePayByID(@PathVariable Long id) {
+		if (!payRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		payRepository.deleteById(id);
+		return ResponseEntity.ok("Successfully delete pay.");
 	}
 }
